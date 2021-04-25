1af4eb749b6030a5532a235d27a49223b378d4e9
NonSleeping <63088467+wakeup208@users.noreply.github.com>
2021-04-26 00:00:54 +0700

    Initial commit

---
+++ app/src/main/java/com/smarteist/imageslider/Config.java
@@ -0,0 +1,12 @@
+package com.smarteist.imageslider;
+
+import com.smarteist.imageslider.R;
+
+public class Config {
+
+    static String[] colorList = {"#008B8B", "#00FF00", "#48D1CC", "#556B2F", "#696969", "#6B8E23", "#8FBC8F", "#AFEEEE", "#B8860B", "#BDB76B", "#D8BFD8", "#DEB887", "#FFFF00", "#FFF0F5", "#EE82EE", "#DC143C", "#C0C0C0", "#FFFFFF"};
+    static int timmerDeley = 300;
+    public static int[] imageId_blur = {R.drawable.wp_0_blur, R.drawable.wp_1_blur, R.drawable.wp_2_blur, R.drawable.wp_3_blur, R.drawable.wp_4_blur, R.drawable.wp_5_blur, R.drawable.wp_6_blur, R.drawable.wp_7_blur, R.drawable.wp_8_blur};
+    public static String[] des = {"0", "1", "2", "3", "4", "5", "6", "7", "8", };
+
+}
